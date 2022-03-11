3b1acab452f0aabecb0fd984e5f372242f73d510
M1Al3x <kasych@ualberta.ca>
2022-03-10 22:22:01 -0600

    1st test

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -1,24 +1,23 @@
-import com.example.simpleparadox.listycity.City;
-import com.example.simpleparadox.listycity.CustomList;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import android.util.Log;
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
     private CustomList list;
 
-    @BeforeAll
+    @BeforeEach
     public void createList(){
         list = new CustomList(null, new ArrayList<>());
     }
 
     @Test
     public void addCityTest(){
-        list = new CustomList(null, new ArrayList<>());
         int listSize = list.getCount();
         list.addCity(new City("Halifax", "NS"));
         assertEquals(list.getCount(), listSize+1);
